da9a7864daea26dabeff444c147dcbe764a43cd5
Javad <ja7ad@live.com>
2022-10-07 10:48:16 +0330

    update: name of forker struct

--- options.go
+++ options.go
@@ -1,17 +1,17 @@
 package forker
 
-type Option func(f *Forker)
+type Option func(f *Fork)
 
 // WithReusePort enable reuse port option for windows
 func WithReusePort(reusePort bool) Option {
-	return func(f *Forker) {
+	return func(f *Fork) {
 		f.ReusePort = reusePort
 	}
 }
 
 // WithCustomNetwork set network type listing type
 func WithCustomNetwork(network Network) Option {
-	return func(f *Forker) {
+	return func(f *Fork) {
 		f.Network = network
 	}
 }
